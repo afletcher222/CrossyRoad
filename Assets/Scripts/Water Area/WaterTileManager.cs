78c243efb312a006df51080f369b4d5bbdfd53a3
Jonah T <55107306+jmthomas6@users.noreply.github.com>
2019-09-25 15:28:32 -0400

    Water Progress 1

---
+++ Assets/Scripts/Water Area/WaterTileManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WaterType { pad, log }
+
+public class WaterTileManager : MonoBehaviour
+{
+    public WaterType type;
+
+    private void Awake()
+    {
+        if (Random.value > .5)
+        {
+            type = WaterType.pad;
+        }
+        else
+        {
+            type = WaterType.log;
+        }
+    }
+}
