6c3b5faf4682aaea2f0a622b8852cc3381058ce2
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-09-25 14:13:20 -0400

    fixed merge issues

--- Assets/Scripts/CameraScript/CameraScript.cs
+++ Assets/Scripts/CameraScript/CameraScript.cs
@@ -8,25 +8,16 @@ public class CameraScript : MonoBehaviour
 
     public float smoothTime = 0.3f;
 
-<<<<<<< HEAD
+
     public Vector3 velocity = Vector3.zero;
-=======
-    // Start is called before the first frame update
-    void Start()
-    {
-       //mainCamera.transform.LookAt(cameraLookTarget);
-    }
->>>>>>> fc52f16afa7d864e2ed1fe7cee78d7e87fe33e6a
+
 
     // Update is called once per frame
     void LateUpdate()
     {
-<<<<<<< HEAD
         Vector3 pos = target.position;
         pos.y = transform.position.y;
         transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smoothTime);
-=======
-      // mainCamera.transform.LookAt(cameraLookTarget);
->>>>>>> fc52f16afa7d864e2ed1fe7cee78d7e87fe33e6a
+
     }
 }
