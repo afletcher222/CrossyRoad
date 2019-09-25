d484725c45f32a316d2c015a7e9ace90f4444e5a
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-09-25 15:02:18 -0400

    Fixed spawning

--- Assets/Scripts/PlatformSpawn/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawn/PlatformSpawner.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     public List<Transform> platformList;
-    public int listCounter = 0;
-    public int amountPooled = 10;
 
     public Transform grassPlatform;
     public Transform waterPlatform;
@@ -17,7 +15,7 @@ public class PlatformSpawner : MonoBehaviour
     public Vector3 startLocation = new Vector3 (0,0,0);
     public Vector3 spawnLocation;
 
-    public int numberOfPlatforms = 10;
+    public int numberOfPlatforms = 14;
     public int platformsToRemove = 0;
     public int removeAmount;
 
@@ -26,7 +24,7 @@ public class PlatformSpawner : MonoBehaviour
     void Start()
     {
         platformList = new List<Transform>();
-        removeAmount = 3;
+        removeAmount = 7;
         startLocation = spawnLocation;
 
         for (int i = 0; i < numberOfPlatforms; i++)
@@ -41,19 +39,16 @@ public class PlatformSpawner : MonoBehaviour
                         Transform grass = Instantiate(grassPlatform, spawnLocation, Quaternion.identity);
                         grass.SetParent(platformParent);
                         platformList.Add(grass);
-                        listCounter++;
                         break;
                     case 2:
                         Transform water = Instantiate(waterPlatform, spawnLocation, Quaternion.identity);
                         water.SetParent(platformParent);
                         platformList.Add(water);
-                        listCounter++;
                         break;
                     case 3:
                         Transform road = Instantiate(roadPlatform, spawnLocation, Quaternion.identity);
                         road.SetParent(platformParent);
                         platformList.Add(road);
-                        listCounter++;
                         break;
                 }
 
@@ -61,8 +56,20 @@ public class PlatformSpawner : MonoBehaviour
             else if(i == 0)
             {
                 Transform grass = Instantiate(grassPlatform, spawnLocation, Quaternion.identity);
+                grass.SetParent(platformParent);
                 platformList.Add(grass);
-                listCounter++;
+                grass = Instantiate(grassPlatform, (spawnLocation - new Vector3(0,0,1)), Quaternion.identity);
+                grass.SetParent(platformParent);
+                platformList.Add(grass);
+                grass = Instantiate(grassPlatform, (spawnLocation - new Vector3(0, 0, 2)), Quaternion.identity);
+                grass.SetParent(platformParent);
+                platformList.Add(grass);
+                grass = Instantiate(grassPlatform, (spawnLocation - new Vector3(0, 0, 3)), Quaternion.identity);
+                grass.SetParent(platformParent);
+                platformList.Add(grass);
+                grass = Instantiate(grassPlatform, (spawnLocation - new Vector3(0, 0, 4)), Quaternion.identity);
+                grass.SetParent(platformParent);
+                platformList.Add(grass);
             }
             spawnLocation.z++;
         }
@@ -87,19 +94,16 @@ public class PlatformSpawner : MonoBehaviour
                         Transform grass = Instantiate(grassPlatform, spawnLocation, Quaternion.identity);
                         grass.SetParent(platformParent);
                         platformList.Add(grass);
-                        listCounter++;
                         break;
                     case 2:
                         Transform water = Instantiate(waterPlatform, spawnLocation, Quaternion.identity);
                         water.SetParent(platformParent);
                         platformList.Add(water);
-                        listCounter++;
                         break;
                     case 3:
                         Transform road = Instantiate(roadPlatform, spawnLocation, Quaternion.identity);
                         road.SetParent(platformParent);
                         platformList.Add(road);
-                        listCounter++;
                         break;
                 }
             spawnLocation.z++;
@@ -113,8 +117,6 @@ public class PlatformSpawner : MonoBehaviour
             Destroy(platformList[platformsToRemove].gameObject);
             platformList.Remove(platformList[platformsToRemove]);
         }
-
-        removeAmount = 7;
     }
 }
 
