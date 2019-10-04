b0f00fb944a18da6bb6b27c5dd400acd8bd54a52
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-10-04 16:15:28 -0400

    added scoring

---
+++ Assets/Scripts/Scoring/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameController : MonoBehaviour
+{
+    public int[] highScores = new int[10];
+
+
+    private GameData CreateSaveDataObject()
+    {
+        GameData save = new GameData();
+        for (int i = 0; i < highScores.Length; i++)
+        {
+            save.highScores[i] = highScores[i];
+        }
+        return save;
+    }
+
+    public void SaveHighScores(int score)
+    {
+        //Use the score to update the highScores array of the game controller
+        //In the code below, I am using hard coded values to show you how the saving process works
+        //Your code should add the score in the highScores array of the game controller
+        highScores[0] = 10;
+        highScores[1] = 20;
+        highScores[2] = 30;
+        highScores[3] = 40;
+        highScores[4] = 50;
+        highScores[5] = 65;
+
+        GameData save = CreateSaveDataObject();
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gameSave.save");
+        bf.Serialize(file, save);
+        file.Close();
+    }
+
+    public void ReadHighScores()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gameSave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameSave.save", FileMode.Open);
+            GameData save = (GameData)bf.Deserialize(file);
+            file.Close();
+
+            for (int i = 0; i < highScores.Length; i++)
+            {
+                highScores[i] = save.highScores[i];
+            }
+        }
+    }
+
+
+}
