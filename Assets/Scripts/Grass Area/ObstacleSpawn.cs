f4365fcf1c6daad26b8d9abf23c945068c97dabf
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-10-07 15:46:26 -0400

    added objects outside of play area

--- Assets/Scripts/Grass Area/ObstacleSpawn.cs
+++ Assets/Scripts/Grass Area/ObstacleSpawn.cs
@@ -6,6 +6,7 @@ public class ObstacleSpawn : MonoBehaviour
 {
     public GameObject[] obstacles = new GameObject[4];
     public List<ObstacleLocations> locations = new List<ObstacleLocations>();
+    public List<ObstacleLocations> outsideLocations = new List<ObstacleLocations>();
 
     // Start is called before the first frame update
     void Awake()
@@ -16,14 +17,38 @@ public class ObstacleSpawn : MonoBehaviour
 
         for (int i = 0; i < random; i++)
         {
-            int randomObstacles = Random.Range(1, obstacles.Length);
-            int randomLocations = Random.Range(1, locations.Count);
+            int randomObstacles = Random.Range(0, (obstacles.Length - 1));
+            int randomLocations = Random.Range(0, (locations.Count - 1));
 
             GameObject obstacle = Instantiate(obstacles[randomObstacles], locations[randomLocations].transform.position, locations[randomLocations].transform.rotation);
             obstacle.transform.parent = gameObject.transform;
 
             Destroy(locations[randomLocations].GetComponent<ParentLocation>());
             locations.Remove(locations[randomLocations]);
+
+        }
+
+        random = Random.Range(6, 14);
+
+        for (int i = 0; i < random; i++)
+        {
+            int randomObstacles = Random.Range(0, (obstacles.Length - 1));
+            int randomLocations = Random.Range(0, (outsideLocations.Count - 1));
+
+            if (i > 1)
+            {
+                GameObject obstacle = Instantiate(obstacles[randomObstacles], locations[randomLocations].transform.position, locations[randomLocations].transform.rotation);
+                obstacle.transform.parent = gameObject.transform;
+                Destroy(locations[randomLocations].GetComponent<ParentLocation>());
+                locations.Remove(locations[randomLocations]);
+            }
+            else if( i <= 1)
+            {
+                GameObject obstacle = Instantiate(obstacles[randomObstacles], outsideLocations[i].transform.position, outsideLocations[i].transform.rotation);
+                obstacle.transform.parent = gameObject.transform;
+                Destroy(locations[randomLocations].GetComponent<ParentLocation>());
+                locations.Remove(locations[randomLocations]);
+            }
 
         }
     }
