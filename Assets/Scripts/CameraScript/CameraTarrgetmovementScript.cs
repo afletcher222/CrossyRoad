1b1880abb2a7a51c994237c2ea11798ee6b9a0cb
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-10-23 14:19:48 -0400

    Camera movement

--- Assets/Scripts/CameraScript/CameraTarrgetmovementScript.cs
+++ Assets/Scripts/CameraScript/CameraTarrgetmovementScript.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class CameraTarrgetmovementScript : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject playerCameraTarget;
     public float speed;
     public float offset;
     public float horizontalOffset;
+    public PlayerMove player;
+    Vector3 newPosition;
     // Start is called before the first frame update
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        Vector3 newPosition = transform.position;
-        newPosition.x = player.transform.position.x + horizontalOffset;
-        newPosition += (transform.forward * speed * Time.deltaTime);
-        transform.position = newPosition;
-        if(transform.position.z < player.transform.position.z)
+        if (player.cameraMove > 3)
         {
-            newPosition.z = player.transform.position.z + offset;
+            newPosition = transform.position;
+            newPosition.x = playerCameraTarget.transform.position.x + horizontalOffset;
+            newPosition += (transform.forward * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+        if(newPosition.z < playerCameraTarget.transform.position.z)
+        {
+            newPosition = transform.position;
+            newPosition.z = playerCameraTarget.transform.position.z + offset;
             transform.position = newPosition;
         }
     }
